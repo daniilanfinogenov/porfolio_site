7c05fc53b247e86d63cd9d55078e9d0e18476024
Daniel Anfinogenov <society27182818@gmail.com>
2022-10-08 20:06:03 +0300

    version 2.1

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
 
 @app.route("/")
 def home():
@@ -10,6 +12,3 @@ def home():
 @app.route("/resume")
 def resume():
     return "<p>comming soon...</p>"
-
-
-
